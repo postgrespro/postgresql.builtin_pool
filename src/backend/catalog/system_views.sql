598dcd7307f3a3d1a246e13d43281b749dfe44f1
Konstantin Knizhnik <knizhnik@garret.ru>
2018-12-04 18:42:26 +0300

    Change column names in snapfs_snapshots

--- src/backend/catalog/system_views.sql
+++ src/backend/catalog/system_views.sql
@@ -1119,9 +1119,9 @@ LANGUAGE INTERNAL
 STRICT IMMUTABLE PARALLEL SAFE
 AS 'jsonb_insert';
 
-CREATE VIEW snapfs_snapshots AS 
-select *,(select pg_get_snapshot_timestamp(generate_series)), pg_size_pretty((select pg_get_snapshot_size(generate_series)))
-from generate_series((select oldest_snapshot from pg_control_snapshot()), (select recent_snapshot from pg_control_snapshot()));
+CREATE VIEW snapfs_snapshots AS
+select *,(select pg_get_snapshot_timestamp(generate_series)) as snap_created, pg_size_pretty((select pg_get_snapshot_size(generate_series))) as snap_size
+from generate_series((select oldest_snapshot from pg_control_snapshot()), (select recent_snapshot from pg_control_snapshot())) as snap_id;
 
 --
 -- The default permissions for functions mean that anyone can execute them.
